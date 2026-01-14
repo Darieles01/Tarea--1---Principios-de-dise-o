9c3d19905cf45a181879e3a985a613a445c96acc
Darieles01 <xxdarilesxx@gmail.com>
2026-01-14 17:58:28 -0600

    Añadir clase Main para ejecutar ejemplos de Car y Shape, demostrando la funcionalidad de inyección de dependencias y cálculo de áreas.

--- solid/dependency/fixed/CarFixed.java
+++ solid/dependency/fixed/CarFixed.java
@@ -32,3 +32,15 @@ class ElectricEngine implements IEngine {
 
     
 }
+
+class Main {
+    public static void main(String[] args) {
+        IEngine dieselEngine = new DisielEngine();
+        Car dieselCar = new Car(dieselEngine);
+        dieselCar.start();
+
+        IEngine electricEngine = new ElectricEngine();
+        Car electricCar = new Car(electricEngine);
+        electricCar.start();
+    }
+}
